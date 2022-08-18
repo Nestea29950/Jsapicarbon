94dee8bf8f2e3861b7d3d096aaa15c2b0822a0d0
Nestea <72440100+Nestea29950@users.noreply.github.com>
2022-08-18 10:25:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ app.use(cors({
   origin: '*'
 }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.get('/api', function (req, res) {
 
@@ -31,66 +28,65 @@ app.get('/api', function (req, res) {
       console.log('performancescore:', data2.data.lighthouseResult.categories.performance.score);
       console.log(carbon);
       var audits = [ {"id":1,"name":"carbon","score":carbon}
-      , {"id":2,"name":"performances","score":data2.data.lighthouseResult.categories.performance.score}
-      , {"id":3,"name":"first-contentful-paint-3g","score":data2.data.lighthouseResult.audits['first-contentful-paint-3g'].score}
-      , {"id":4,"name":"interactive","score":data2.data.lighthouseResult.audits['interactive'].score}
-      , {"id":5,"name":"preload-lcp-image","score":data2.data.lighthouseResult.audits['preload-lcp-image'].score}
-      , {"id":6,"name":"main-thread-tasks","score":data2.data.lighthouseResult.audits['main-thread-tasks'].score}
-      , {"id":7,"name":"unused-css-rules","score":data2.data.lighthouseResult.audits['unused-css-rules'].score}
-      , {"id":8,"name":"viewport","score":data2.data.lighthouseResult.audits['viewport'].score}
-      , {"id":9,"name":"user-timings","score":data2.data.lighthouseResult.audits['user-timings'].score}
-      , {"id":10,"name":"final-screenshot","score":data2.data.lighthouseResult.audits['final-screenshot'].score}
-      , {"id":11,"name":"bootup-time","score":data2.data.lighthouseResult.audits['bootup-time'].score}
-      , {"id":12,"name":"non-composited-animations","score":data2.data.lighthouseResult.audits['non-composited-animations'].score}
-      , {"id":13,"name":"third-party-summary","score":data2.data.lighthouseResult.audits['third-party-summary'].score}
-      , {"id":14,"name":"diagnostics","score":data2.data.lighthouseResult.audits['diagnostics'].score}
-      , {"id":15,"name":"metrics","score":data2.data.lighthouseResult.audits['metrics'].score}
-      , {"id":16,"name":"no-document-write","score":data2.data.lighthouseResult.audits['no-document-write'].score}
-      , {"id":17,"name":"performance-budget","score":data2.data.lighthouseResult.audits['performance-budget'].score}
-      , {"id":18,"name":"largest-contentful-paint","score":data2.data.lighthouseResult.audits['largest-contentful-paint'].score}
-      , {"id":19,"name":'offscreen-images',"score": data2.data.lighthouseResult.audits['offscreen-images'].score}
-      , {"id":20,"name":'screenshot-thumbnails',"score": data2.data.lighthouseResult.audits['screenshot-thumbnails'].score}
-      , {"id":21,"name":'critical-request-chains',"score": data2.data.lighthouseResult.audits['critical-request-chains'].score}
-      , {"id":22,"name":'unminified-javascript',"score": data2.data.lighthouseResult.audits['unminified-javascript'].score}
-      , {"id":23,"name":'uses-responsive-images',"score": data2.data.lighthouseResult.audits['uses-responsive-images'].score}
-      , {"id":24,"name":'timing-budget',"score": data2.data.lighthouseResult.audits['timing-budget'].score}
-      , {"id":25,"name":'unused-javascript',"score": data2.data.lighthouseResult.audits['unused-javascript'].score}
-      , {"id":26,"name":'first-meaningful-paint',"score": data2.data.lighthouseResult.audits['first-meaningful-paint'].score}
-      , {"id":27,"name":'lcp-lazy-loaded',"score": data2.data.lighthouseResult.audits['lcp-lazy-loaded'].score}
-      , {"id":28,"name":'network-requests',"score": data2.data.lighthouseResult.audits['network-requests'].score} 
-      , {"id":29,"name":'uses-long-cache-ttl',"score": data2.data.lighthouseResult.audits['uses-long-cache-ttl'].score}
-      , {"id":30,"name":'layout-shift-elements',"score": data2.data.lighthouseResult.audits['layout-shift-elements'].score}
-      , {"id":31,"name":'redirects',"score": data2.data.lighthouseResult.audits['redirects'].score}
-      , {"id":32,"name":'resource-summary',"score": data2.data.lighthouseResult.audits['resource-summary'].score}
-      , {"id":33,"name":'network-rtt',"score": data2.data.lighthouseResult.audits['network-rtt'].score}
-      , {"id":34,"name":'unminified-css',"score": data2.data.lighthouseResult.audits['unminified-css'].score}
-      , {"id":35,"name":'efficient-animated-content',"score": data2.data.lighthouseResult.audits['efficient-animated-content'].score}
-      , {"id":36,"name":'uses-passive-event-listeners',"score": data2.data.lighthouseResult.audits['uses-passive-event-listeners'].score}
-      , {"id":37,"name":'cumulative-layout-shift',"score": data2.data.lighthouseResult.audits['cumulative-layout-shift'].score}
-      , {"id":38,"name":'server-response-time',"score": data2.data.lighthouseResult.audits['server-response-time'].score}
-      , {"id":39,"name":'mainthread-work-breakdown',"score": data2.data.lighthouseResult.audits['mainthread-work-breakdown'].score}
-      , {"id":40,"name":'largest-contentful-paint-element',"score": data2.data.lighthouseResult.audits['largest-contentful-paint-element'].score}
-      , {"id":41,"name":'font-display',"score": data2.data.lighthouseResult.audits['font-display'].score}
-      , {"id":42,"name":'total-blocking-time',"score": data2.data.lighthouseResult.audits['total-blocking-time'].score}
-      , {"id":43,"name":'full-page-screenshot',"score": data2.data.lighthouseResult.audits['full-page-screenshot'].score}
-      , {"id":44,"name":'total-byte-weight',"score": data2.data.lighthouseResult.audits['total-byte-weight'].score}
-      , {"id":45,"name":'long-tasks',"score": data2.data.lighthouseResult.audits['long-tasks'].score}
-      , {"id":46,"name":'network-server-latency',"score": data2.data.lighthouseResult.audits['network-server-latency'].score}
-      , {"id":47,"name":'third-party-facades',"score": data2.data.lighthouseResult.audits['third-party-facades'].score}
-      , {"id":48,"name":'max-potential-fid',"score": data2.data.lighthouseResult.audits['max-potential-fid'].score}
-      , {"id":49,"name":'speed-index',"score": data2.data.lighthouseResult.audits['speed-index'].score}
-      , {"id":50,"name":'no-unload-listeners',"score": data2.data.lighthouseResult.audits['no-unload-listeners'].score}
-      , {"id":51,"name":'uses-text-compression',"score": data2.data.lighthouseResult.audits['uses-text-compression'].score}
-      , {"id":52,"name":'unsized-images',"score": data2.data.lighthouseResult.audits['unsized-images'].score}
-      , {"id":53,"name":'duplicated-javascript',"score": data2.data.lighthouseResult.audits['duplicated-javascript'].score}
-      , {"id":54,"name":'dom-size',"score": data2.data.lighthouseResult.audits['dom-size'].score}
-      , {"id":55,"name":'uses-optimized-images',"score": data2.data.lighthouseResult.audits['uses-optimized-images'].score}
-      , {"id":56,"name":"performances","score":data2.data.lighthouseResult.categories.performance.score}
-      , {"id":57,"name":'render-blocking-resources',"score": data2.data.lighthouseResult.audits['render-blocking-resources'].score}
-      , {"id":58,"name":'modern-image-formats',"score": data2.data.lighthouseResult.audits['modern-image-formats'].score}
-      , {"id":59,"name":'uses-rel-preload',"score": data2.data.lighthouseResult.audits['uses-rel-preload'].score}
-      , {"id":60,"name":'legacy-javascript',"score": data2.data.lighthouseResult.audits['legacy-javascript'].score}
-      , {"id":61,"name":'first-contentful-paint',"score": data2.data.lighthouseResult.audits['first-contentful-paint'].score}];
+      , {"id":2,"name":"performances","fr":"traduction","score":data2.data.lighthouseResult.categories.performance.score}
+      , {"id":3,"name":"first-contentful-paint-3g","score":data2.data.lighthouseResult.audits['first-contentful-paint-3g'].score,"description":data2.data.lighthouseResult.audits['first-contentful-paint-3g'].description,}
+      , {"id":4,"name":"interactive","score":data2.data.lighthouseResult.audits['interactive'].score,"description":data2.data.lighthouseResult.audits['interactive'].description}
+      , {"id":5,"name":"preload-lcp-image","score":data2.data.lighthouseResult.audits['preload-lcp-image'].score,"description":data2.data.lighthouseResult.audits['preload-lcp-image'].description}
+      , {"id":6,"name":"main-thread-tasks","score":data2.data.lighthouseResult.audits['main-thread-tasks'].score,"description":data2.data.lighthouseResult.audits['main-thread-tasks'].description}
+      , {"id":7,"name":"unused-css-rules","score":data2.data.lighthouseResult.audits['unused-css-rules'].score,"description":data2.data.lighthouseResult.audits['unused-css-rules'].description}
+      , {"id":8,"name":"viewport","score":data2.data.lighthouseResult.audits['viewport'].score,"description":data2.data.lighthouseResult.audits['viewport'].description}
+      , {"id":9,"name":"user-timings","score":data2.data.lighthouseResult.audits['user-timings'].score,"description":data2.data.lighthouseResult.audits['user-timings'].description}
+      , {"id":10,"name":"final-screenshot","score":data2.data.lighthouseResult.audits['final-screenshot'].score,"description":data2.data.lighthouseResult.audits['final-screenshot'].description}
+      , {"id":11,"name":"bootup-time","score":data2.data.lighthouseResult.audits['bootup-time'].score,"description":data2.data.lighthouseResult.audits['bootup-time'].description}
+      , {"id":12,"name":"non-composited-animations","score":data2.data.lighthouseResult.audits['non-composited-animations'].score,"description":data2.data.lighthouseResult.audits['non-composited-animations'].description}
+      , {"id":13,"name":"third-party-summary","score":data2.data.lighthouseResult.audits['third-party-summary'].score,"description":data2.data.lighthouseResult.audits['third-party-summary'].description}
+      , {"id":14,"name":"diagnostics","score":data2.data.lighthouseResult.audits['diagnostics'].score,"description":data2.data.lighthouseResult.audits['diagnostics'].description}
+      , {"id":15,"name":"metrics","score":data2.data.lighthouseResult.audits['metrics'].score,"description":data2.data.lighthouseResult.audits['metrics'].description}
+      , {"id":16,"name":"no-document-write","score":data2.data.lighthouseResult.audits['no-document-write'].score,"description":data2.data.lighthouseResult.audits['no-document-write'].description}
+      , {"id":17,"name":"performance-budget","score":data2.data.lighthouseResult.audits['performance-budget'].score,"description":data2.data.lighthouseResult.audits['performance-budget'].description}
+      , {"id":18,"name":"largest-contentful-paint","score":data2.data.lighthouseResult.audits['largest-contentful-paint'].score,"description":data2.data.lighthouseResult.audits['largest-contentful-paint'].description}
+      , {"id":19,"name":'offscreen-images',"score": data2.data.lighthouseResult.audits['offscreen-images'].score,"description":data2.data.lighthouseResult.audits['offscreen-images'].description}
+      , {"id":20,"name":'screenshot-thumbnails',"score": data2.data.lighthouseResult.audits['screenshot-thumbnails'].score,"description":data2.data.lighthouseResult.audits['screenshot-thumbnails'].description}
+      , {"id":21,"name":'critical-request-chains',"score": data2.data.lighthouseResult.audits['critical-request-chains'].score,"description":data2.data.lighthouseResult.audits['critical-request-chains'].description}
+      , {"id":22,"name":'unminified-javascript',"score": data2.data.lighthouseResult.audits['unminified-javascript'].score,"description":data2.data.lighthouseResult.audits['unminified-javascript'].description}
+      , {"id":23,"name":'uses-responsive-images',"score": data2.data.lighthouseResult.audits['uses-responsive-images'].score,"description":data2.data.lighthouseResult.audits['uses-responsive-images'].description}
+      , {"id":24,"name":'timing-budget',"score": data2.data.lighthouseResult.audits['timing-budget'].score,"description":data2.data.lighthouseResult.audits['timing-budget'].description}
+      , {"id":25,"name":'unused-javascript',"score": data2.data.lighthouseResult.audits['unused-javascript'].score,"description":data2.data.lighthouseResult.audits['unused-javascript'].description}
+      , {"id":26,"name":'first-meaningful-paint',"score": data2.data.lighthouseResult.audits['first-meaningful-paint'].score,"description":data2.data.lighthouseResult.audits['first-meaningful-paint'].description}
+      , {"id":27,"name":'lcp-lazy-loaded',"score": data2.data.lighthouseResult.audits['lcp-lazy-loaded'].score,"description":data2.data.lighthouseResult.audits['lcp-lazy-loaded'].description}
+      , {"id":28,"name":'network-requests',"score": data2.data.lighthouseResult.audits['network-requests'].score,"description":data2.data.lighthouseResult.audits['network-requests'].description} 
+      , {"id":29,"name":'uses-long-cache-ttl',"score": data2.data.lighthouseResult.audits['uses-long-cache-ttl'].score,"description":data2.data.lighthouseResult.audits['uses-long-cache-ttl'].description}
+      , {"id":30,"name":'layout-shift-elements',"score": data2.data.lighthouseResult.audits['layout-shift-elements'].score,"description":data2.data.lighthouseResult.audits['layout-shift-elements'].description}
+      , {"id":31,"name":'redirects',"score": data2.data.lighthouseResult.audits['redirects'].score,"description":data2.data.lighthouseResult.audits['redirects'].description}
+      , {"id":32,"name":'resource-summary',"score": data2.data.lighthouseResult.audits['resource-summary'].score,"description":data2.data.lighthouseResult.audits['resource-summary'].description}
+      , {"id":33,"name":'network-rtt',"score": data2.data.lighthouseResult.audits['network-rtt'].score,"description":data2.data.lighthouseResult.audits['network-rtt'].description}
+      , {"id":34,"name":'unminified-css',"score": data2.data.lighthouseResult.audits['unminified-css'].score,"description":data2.data.lighthouseResult.audits['unminified-css'].description}
+      , {"id":35,"name":'efficient-animated-content',"score": data2.data.lighthouseResult.audits['efficient-animated-content'].score,"description":data2.data.lighthouseResult.audits['efficient-animated-content'].description}
+      , {"id":36,"name":'uses-passive-event-listeners',"score": data2.data.lighthouseResult.audits['uses-passive-event-listeners'].score,"description":data2.data.lighthouseResult.audits['uses-passive-event-listeners'].description}
+      , {"id":37,"name":'cumulative-layout-shift',"score": data2.data.lighthouseResult.audits['cumulative-layout-shift'].score,"description":data2.data.lighthouseResult.audits['cumulative-layout-shift'].description}
+      , {"id":38,"name":'server-response-time',"score": data2.data.lighthouseResult.audits['server-response-time'].score,"description":data2.data.lighthouseResult.audits['server-response-time'].description}
+      , {"id":39,"name":'mainthread-work-breakdown',"score": data2.data.lighthouseResult.audits['mainthread-work-breakdown'].score,"description":data2.data.lighthouseResult.audits['mainthread-work-breakdown'].description}
+      , {"id":40,"name":'largest-contentful-paint-element',"score": data2.data.lighthouseResult.audits['largest-contentful-paint-element'].score,"description":data2.data.lighthouseResult.audits['largest-contentful-paint-element'].description}
+      , {"id":41,"name":'font-display',"score": data2.data.lighthouseResult.audits['font-display'].score,"description":data2.data.lighthouseResult.audits['font-display'].description}
+      , {"id":42,"name":'total-blocking-time',"score": data2.data.lighthouseResult.audits['total-blocking-time'].score,"description":data2.data.lighthouseResult.audits['total-blocking-time'].description}
+      , {"id":43,"name":'full-page-screenshot',"score": data2.data.lighthouseResult.audits['full-page-screenshot'].score,"description":data2.data.lighthouseResult.audits['full-page-screenshot'].description}
+      , {"id":44,"name":'total-byte-weight',"score": data2.data.lighthouseResult.audits['total-byte-weight'].score,"description":data2.data.lighthouseResult.audits['total-byte-weight'].description}
+      , {"id":45,"name":'long-tasks',"score": data2.data.lighthouseResult.audits['long-tasks'].score,"description":data2.data.lighthouseResult.audits['long-tasks'].description}
+      , {"id":46,"name":'network-server-latency',"score": data2.data.lighthouseResult.audits['network-server-latency'].score,"description":data2.data.lighthouseResult.audits['network-server-latency'].description}
+      , {"id":47,"name":'third-party-facades',"score": data2.data.lighthouseResult.audits['third-party-facades'].score,"description":data2.data.lighthouseResult.audits['third-party-facades'].description}
+      , {"id":48,"name":'max-potential-fid',"score": data2.data.lighthouseResult.audits['max-potential-fid'].score,"description":data2.data.lighthouseResult.audits['max-potential-fid'].description}
+      , {"id":49,"name":'speed-index',"score": data2.data.lighthouseResult.audits['speed-index'].score,"description":data2.data.lighthouseResult.audits['speed-index'].description}
+      , {"id":50,"name":'no-unload-listeners',"score": data2.data.lighthouseResult.audits['no-unload-listeners'].score,"description":data2.data.lighthouseResult.audits['no-unload-listeners'].description}
+      , {"id":51,"name":'uses-text-compression',"score": data2.data.lighthouseResult.audits['uses-text-compression'].score,"description":data2.data.lighthouseResult.audits['uses-text-compression'].description}
+      , {"id":52,"name":'unsized-images',"score": data2.data.lighthouseResult.audits['unsized-images'].score,"description":data2.data.lighthouseResult.audits['unsized-images'].description}
+      , {"id":53,"name":'duplicated-javascript',"score": data2.data.lighthouseResult.audits['duplicated-javascript'].score,"description":data2.data.lighthouseResult.audits['duplicated-javascript'].description}
+      , {"id":54,"name":'dom-size',"score": data2.data.lighthouseResult.audits['dom-size'].score,"description":data2.data.lighthouseResult.audits['dom-size'].description}
+      , {"id":55,"name":'uses-optimized-images',"score": data2.data.lighthouseResult.audits['uses-optimized-images'].score,"description":data2.data.lighthouseResult.audits['uses-optimized-images'].description}
+      , {"id":56,"name":'render-blocking-resources',"score": data2.data.lighthouseResult.audits['render-blocking-resources'].score,"description":data2.data.lighthouseResult.audits['render-blocking-resources'].description}
+      , {"id":57,"name":'modern-image-formats',"score": data2.data.lighthouseResult.audits['modern-image-formats'].score,"description":data2.data.lighthouseResult.audits['modern-image-formats'].description}
+      , {"id":58,"name":'uses-rel-preload',"score": data2.data.lighthouseResult.audits['uses-rel-preload'].score,"description":data2.data.lighthouseResult.audits['uses-rel-preload'].description}
+      , {"id":59,"name":'legacy-javascript',"score": data2.data.lighthouseResult.audits['legacy-javascript'].score,"description":data2.data.lighthouseResult.audits['legacy-javascript'].description}
+      , {"id":60,"name":'first-contentful-paint',"score": data2.data.lighthouseResult.audits['first-contentful-paint'].score,"description":data2.data.lighthouseResult.audits['first-contentful-paint'].description}];
   
       res.send({
         audits,
