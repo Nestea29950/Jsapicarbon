5a4cc044e1795ece2b6b925b223601b26bfe55ce
Nestea <72440100+Nestea29950@users.noreply.github.com>
2022-08-29 13:25:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
-
 import psi from 'psi';
 import { WebsiteCarbonCalculator, WebsiteCarbonCalculatorError, } from 'website-carbon-calculator';
-
 var app = express();
 import cors from 'cors';
 
@@ -10,12 +8,11 @@ app.use(cors({
   origin: '*'
 }));
 
-
 app.get('/api', function (req, res) {
 
   const url = req.query.url;
   const websiteCarbonCalculator = new WebsiteCarbonCalculator({ pagespeedApiKey: 'AIzaSyCXkSBXLSiY0OlTpgAp0u5OvOrCTVCgveg' });
-
+  // Asynchrone function permettre d'attendre l'appelle d'api 
   async function calculCarbon() {
     try {
       const carbon = await websiteCarbonCalculator.calculateByURL(url)
@@ -26,27 +23,27 @@ app.get('/api', function (req, res) {
 
       JSON.stringify(carbon);
       console.log(carbon);
-    //
+      // Créations d'une variable audits avec les éléments de la réponse api dedans !
       let audits = [];
-      audits.push({"name": "carbon", "score": carbon.co2PerPageview, "Green": carbon.isGreenHost});
-      audits.push({"name": "greenhost", "score": carbon.isGreenHost });
-      audits.push({"name":"performances","score":data2.data.lighthouseResult.categories.performance.score});
-  
-      for(let i in data2.data.lighthouseResult.audits){
-        audits.push({"name": data2.data.lighthouseResult.audits[i].id, "score": data2.data.lighthouseResult.audits[i].score, "value": data2.data.lighthouseResult.audits[i].displayValue});
-     };
-     console.log(audits);
+      audits.push({ "name": "carbon", "score": carbon.co2PerPageview, "Green": carbon.isGreenHost });
+      audits.push({ "name": "greenhost", "score": carbon.isGreenHost });
+      audits.push({ "name": "performances", "score": data2.data.lighthouseResult.categories.performance.score });
+
+      for (let i in data2.data.lighthouseResult.audits) {
+        audits.push({ "name": data2.data.lighthouseResult.audits[i].id, "score": data2.data.lighthouseResult.audits[i].score, "value": data2.data.lighthouseResult.audits[i].displayValue });
+      };
+      console.log("Résultat envoyé !");
       res.send({
         audits,
       });
     }
+    // Récupere l'erreur et renvoie Url non valide !
     catch (error) {
       console.log(error);
       res.send({
         'Erreur': "Url non valide",
       });
     }
-
   }
   calculCarbon();
 
@@ -55,5 +52,5 @@ app.get('/api', function (req, res) {
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("Exemple app is listening on port http://localhost:${port}oui");
+  console.log("Lancement de l'api !");
 });
